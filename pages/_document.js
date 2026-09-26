c27aeef1d3ab5d37554c022292e648c7b0c52e75
robiyann <jamalbulkin@gmail.com>
2026-09-26 18:04:29 +0700

    perf: set Vercel region to Singapore (sin1), optimize mobile GPU rendering and font loading

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,15 @@ export default function Document() {
   return (
     <Html lang="id">
       <Head>
+        {/* Google Fonts preconnect & stylesheet */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+
         {/* PWA Manifest & Meta Tags */}
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
         <meta name="theme-color" content="#09090b" />
-        
         {/* iOS Support */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
